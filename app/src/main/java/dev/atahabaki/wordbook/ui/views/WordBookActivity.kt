4ceb8dea7471291ec0e36c31e13fd10558bd3440
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-18 14:09:29 +0300

    💩 Don't meddle with Dependency Injection for now...

--- app/src/main/java/dev/atahabaki/wordbook/ui/views/WordBookActivity.kt
+++ app/src/main/java/dev/atahabaki/wordbook/ui/views/WordBookActivity.kt
@@ -2,11 +2,47 @@ package dev.atahabaki.wordbook.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import dev.atahabaki.wordbook.R
+import dev.atahabaki.wordbook.adapters.WordItemAdapter
+import dev.atahabaki.wordbook.data.databases.WordDatabase
+import dev.atahabaki.wordbook.data.entities.WordItem
+import dev.atahabaki.wordbook.data.repositories.WordRepository
+import dev.atahabaki.wordbook.databinding.ActivityWordbookBinding
+import dev.atahabaki.wordbook.ui.viewmodelfactories.WordBookViewModelFactory
+import dev.atahabaki.wordbook.ui.viewmodels.WordBookViewModel
 
 class WordBookActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWordbookBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityWordbookBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val database = WordDatabase(this)
+        val repository = WordRepository(database)
+        val factory = WordBookViewModelFactory(repository)
+        val viewModel = ViewModelProvider(this,factory).get(WordBookViewModel::class.java)
+
+        val adapter = WordItemAdapter(listOf(), viewModel)
+        binding.wordsRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.wordsRecyclerView.adapter = adapter
+
+        viewModel.getAllWords().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        binding.wordsFab.setOnClickListener {
+            addJunkItem(viewModel)
+        }
     }
+
+   private fun addJunkItem(viewModel: WordBookViewModel) {
+       viewModel.insertOrUpdate(WordItem("Merhaba", "Hi!"))
+   }
 }
