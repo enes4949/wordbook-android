4358fecd9153d8ea8b0af11c9c67c1d9d32d6f6a
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-22 23:15:19 +0300

    🚧 viewModelFactory class => Interface... It has lots of benefits...

--- app/src/main/java/dev/atahabaki/wordbook/ui/viewmodelfactories/WordBookViewModelFactory.kt
+++ app/src/main/java/dev/atahabaki/wordbook/ui/viewmodelfactories/WordBookViewModelFactory.kt
@@ -1,14 +1,9 @@
 package dev.atahabaki.wordbook.ui.viewmodelfactories
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import dev.atahabaki.wordbook.data.repositories.WordRepository
+import dagger.assisted.AssistedFactory
 import dev.atahabaki.wordbook.ui.viewmodels.WordBookViewModel
 
-class WordBookViewModelFactory(
-    private val repository: WordRepository
-): ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return WordBookViewModel(repository) as T
-    }
+@AssistedFactory
+interface WordBookViewModelFactory {
+    fun create(): WordBookViewModel
 }
