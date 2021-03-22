af4ca1585b6960b7d8e56f125920bafe21a03932
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-22 23:30:36 +0300

    🎉 removed unused imports, made changes 4 DI...

--- app/src/main/java/dev/atahabaki/wordbook/ui/views/WordBookActivity.kt
+++ app/src/main/java/dev/atahabaki/wordbook/ui/views/WordBookActivity.kt
@@ -3,61 +3,40 @@ package dev.atahabaki.wordbook.ui.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.activity.viewModels
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import dagger.hilt.android.AndroidEntryPoint
 import dev.atahabaki.wordbook.R
-import dev.atahabaki.wordbook.adapters.WordItemAdapter
-import dev.atahabaki.wordbook.data.databases.WordDatabase
 import dev.atahabaki.wordbook.data.entities.WordItem
-import dev.atahabaki.wordbook.data.repositories.WordRepository
 import dev.atahabaki.wordbook.databinding.ActivityWordbookBinding
 import dev.atahabaki.wordbook.ui.viewmodelfactories.WordBookViewModelFactory
 import dev.atahabaki.wordbook.ui.viewmodels.WordBookViewModel
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class WordBookActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWordbookBinding
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
+
+    @Inject
+    lateinit var viewModelFactory: WordBookViewModelFactory
+
+    private val viewModel: WordBookViewModel by viewModels {
+        WordBookViewModel.provideFactory(viewModelFactory)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
         initBottomNav()
 
-        val database = WordDatabase(this)
-        val repository = WordRepository(database)
-        val factory = WordBookViewModelFactory(repository)
-        val viewModel = ViewModelProvider(this,factory).get(WordBookViewModel::class.java)
-
-        val adapter = WordItemAdapter(listOf(), viewModel)
-        binding.wordsRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.wordsRecyclerView.adapter = adapter
-
-        ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                return false
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                viewModel.delete(adapter.getWordAtPosition(viewHolder.adapterPosition))
-                adapter.notifyDataSetChanged()
-            }
-        }).attachToRecyclerView(binding.wordsRecyclerView)
-
-        viewModel.getAllWords().observe(this, Observer {
-            adapter.items = it
-            adapter.notifyDataSetChanged()
-        })
-
         binding.wordsFab.setOnClickListener {
             addJunkItem(viewModel)
+        }
+
+        supportFragmentManager.beginTransaction().also {
+            it.replace(R.id.activity_wordbook_framer, WordBookListFragment())
+            it.commit()
         }
     }
 
