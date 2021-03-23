d98fa7f2d9a8a5c76b866bcef0d6f849410d712c
A. Taha Baki <a.taha.baki@outlook.com>
2021-03-23 18:37:36 +0300

    ✨ added new constants for feedback and coffee links...

--- app/src/main/java/dev/atahabaki/wordbook/utils/Constants.kt
+++ app/src/main/java/dev/atahabaki/wordbook/utils/Constants.kt
@@ -3,5 +3,7 @@ package dev.atahabaki.wordbook.utils
 class Constants {
     companion object {
         val WORDBOOK_DB_NAME = "wordbook.db"
+        val FEEDBACK_LINK = "https://github.com/atahabaki/wordbook-android"
+        val BUYMEACOFFE_LINK = "https://www.buymeacoffee.com/atahabaki"
     }
 }
