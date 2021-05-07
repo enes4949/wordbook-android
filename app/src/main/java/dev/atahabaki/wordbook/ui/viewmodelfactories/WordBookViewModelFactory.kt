d66f090f2a367dca6a7ae44c9a328255ba0271f1
A. Taha Baki <a.taha.baki@outlook.com>
2021-05-07 21:13:53 +0300

    🗑️no need for viewModelFactory

--- app/src/main/java/dev/atahabaki/wordbook/ui/viewmodelfactories/WordBookViewModelFactory.kt
+++
@@ -1,9 +0,0 @@
-package dev.atahabaki.wordbook.ui.viewmodelfactories
-
-import dagger.assisted.AssistedFactory
-import dev.atahabaki.wordbook.ui.viewmodels.WordBookViewModel
-
-@AssistedFactory
-interface WordBookViewModelFactory {
-    fun create(): WordBookViewModel
-}
