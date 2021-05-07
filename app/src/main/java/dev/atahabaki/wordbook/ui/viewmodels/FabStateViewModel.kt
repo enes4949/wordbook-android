b66ef56b1a6bbffff48dc64fc0bd8167079d5bcc
A. Taha Baki <a.taha.baki@outlook.com>
2021-05-07 21:15:10 +0300

    ✨  hiltViewModel for other viewmodels

--- app/src/main/java/dev/atahabaki/wordbook/ui/viewmodels/FabStateViewModel.kt
+++ app/src/main/java/dev/atahabaki/wordbook/ui/viewmodels/FabStateViewModel.kt
@@ -3,8 +3,11 @@ package dev.atahabaki.wordbook.ui.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class FabStateViewModel: ViewModel() {
+@HiltViewModel
+class FabStateViewModel @Inject internal constructor(): ViewModel() {
 
     private var mutableFabState = MutableLiveData<Boolean>()
     val fabState: LiveData<Boolean> get() = mutableFabState
